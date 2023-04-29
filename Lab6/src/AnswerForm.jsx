2763ed1bedd58d78c86ee0d3fd156ee91d51980c
Manuel Fissore <manuel.fissore.37@gmail.com>
2023-04-29 15:45:55 +0200

    Lab finished, can't display string beside the checkbox

--- Lab6/src/AnswerForm.jsx
+++ Lab6/src/AnswerForm.jsx
@@ -9,63 +9,79 @@ function EditOrNewFilm(props) {
     const [favorite, setFavorite] = useState((props.addNewOrEdit=='edit')?props.film.isFavorite:false);
     const [rating, setRating] = useState((props.addNewOrEdit=='edit')?props.film.Rating:0);
     const [date, setDate] = useState((props.addNewOrEdit=='edit')?props.film.Date:dayjs().format('YYYY-MM-DD'));
+    const [error, setError] = useState(false);
 
     function handleAdd(){
-        props.changeAddEditMode('false')
-        props.handleAdd(name, favorite, date, rating);
+        console.log(name)
+        if(name==""||name==''||name==" "||name.lenght==0||name==null){
+            setError(true)
+        }
+        else{
+            setError(false)
+            props.changeAddEditMode('false')
+            props.handleAdd(name, favorite, date, rating);
+        }
     }
 
     function handleSave(){
-        props.changeAddEditMode('false')
-        props.handleSave(props.film.ID, name, favorite, date, rating);
+        if(name==""||name==''||name==" "||name.lenght==0||name==null){
+            setError(true)
+        }
+        else{
+            setError(false)
+            props.changeAddEditMode('false')
+            props.handleSave(props.film.ID, name, favorite, date, rating);
+        }
     }
+
     function handleCancel(){
         props.changeAddEditMode('false')
     }
 
-    return <table>
+    return <Table>
         <tbody>
-            <tr>
-                <td><Form.Group controlId="FilmName">
+            <td>
+            <Row>
+                <Form.Group controlId="FilmName">
                         <Form.Label className='fw-light'>Film Name</Form.Label>
-                        {props.addNewOrEdit=='add' && <Form.Control type="text" onChange={(ev)=>{setName(ev.target.value)}} name="text" placeholder="Enter Film Name" />}
-                        {props.addNewOrEdit=='edit' && <Form.Control type="text" onChange={(ev)=>{setName(ev.target.value)}} name="text" placeholder={props.film.Title} />}
-                    </Form.Group></td>
-
-                <td><Form.Group controlId="Insert watch date">
-                    <Form.Label className='fw-light'>Enter watch date</Form.Label>
-                    {props.addNewOrEdit=='add' && <Form.Control type="date" onChange={(ev)=>{setDate(ev.target.value)}} name="date"/>}
-                    {props.addNewOrEdit=='edit' && <Form.Control type="date" onChange={(ev)=>{setDate(ev.target.value)}} name="date" value={(props.film.Date=='undefined')?'':props.film.Date.format("YYYY-MM-DD")}/>} {/*placeholder={(date=='undefined')?'':String(date.format*(AAAA/MM/DD))}*/}
-
-                </Form.Group></td>
-
-                <td><Form.Group controlId="Rating">
-                    <Form.Label className='fw-light'>Rating</Form.Label>
-                    {props.addNewOrEdit=='add' && <Form.Control type="number" min="0" max="5"  onChange={(ev)=>{setRating(ev.target.value)}} name="rating" placeholder="0" />}
-                    {props.addNewOrEdit=='edit' && <Form.Control type="number" min="0" max="5"  onChange={(ev)=>{setRating(ev.target.value)}} name="rating" placeholder={props.film.Rating} />}
-                </Form.Group></td>
-
-                <td><Form.Group controlId="isFavorite">
-                    <td>
-                    {props.addNewOrEdit=='add' && <Form.Check type="checkbox"  onChange={(ev)=>{setFavorite(ev.target.value)}} name="fav"/>}
-                    {props.addNewOrEdit=='edit' && (props.film.isFavorite==true || props.film.isFavorite=='on') && <Form.Check type="checkbox"  onChange={(ev)=>{setFavorite(ev.target.value)}} name="fav" checked />}
-                    {props.addNewOrEdit=='edit' && !(props.film.isFavorite==true || props.film.isFavorite=='on') && <Form.Check type="checkbox"  onChange={(ev)=>{setFavorite(ev.target.value)}} name="fav"/>}
-                    </td>
-                    <td>
-                    <Form.Label className='fw-light'>is favorite?</Form.Label>
-                     </td>
-                </Form.Group></td>
-                
-                <td><Form.Group controlId="addButton">
-                    <Form.Label className='fw-light'>&nbsp;</Form.Label><br />
+                        {props.addNewOrEdit=='add' && <Form.Control type="text" className={(error==false)?"":"form-square border border-danger"} onChange={(ev)=>{setName(ev.target.value)}} name="Title" placeholder="Enter Film Name" />}
+                        {props.addNewOrEdit=='edit' && <Form.Control type="text" className={(error==false)?"":"form-square border border-danger"} onChange={(ev)=>{setName(ev.target.value)}} name="Title" value={name} />}
+                    </Form.Group>
+            </Row>
+            <Row>
+                <Col xs={8}>
+                    <Form.Group controlId="Insert watch date">
+                        <Form.Label className='fw-light'>Enter watch date</Form.Label>
+                        {props.addNewOrEdit=='add' && <Form.Control type="date" onChange={(ev)=>{setDate(ev.target.value)}} name="date"/>}
+                        {props.addNewOrEdit=='edit' && <Form.Control type="date" onChange={(ev)=>{setDate(ev.target.value)}} name="date" value={(date=='undefined')?'':date.format("YYYY-MM-DD")}/>} {/*placeholder={(date=='undefined')?'':String(date.format*(AAAA/MM/DD))}*/}
+                    </Form.Group>
+                </Col>
+                <Col  xs={4}>
+                    <Form.Group controlId="Rating">
+                        <Form.Label className='fw-light'>Rating</Form.Label>
+                        {props.addNewOrEdit=='add' && <Form.Control type="number" min="0" max="5"  onChange={(ev)=>{setRating(ev.target.value)}} name="rating" placeholder="0" />}
+                        {props.addNewOrEdit=='edit' && <Form.Control type="number" min="0" max="5"  onChange={(ev)=>{setRating(ev.target.value)}} name="rating" value={rating} />}
+                    </Form.Group>
+                </Col>
+            </Row>
+            <Row>
+                <Form.Group controlId="isFavorite">
+                    {props.addNewOrEdit=='add' && <Form.Check type="checkbox" onChange={(ev)=>{setFavorite(ev.target.value)}} name="fav"/>}
+                    {props.addNewOrEdit=='edit' && (favorite==true || favorite=='on') && <Form.Check type="checkbox"  label='is one of your favorite?' onChange={(ev)=>{setFavorite(ev.target.value)}} name="fav" checked>is one of your favorite?</Form.Check>}
+                    {props.addNewOrEdit=='edit' && !(favorite==true || favorite=='on') && <Form.Check type="checkbox" label='is one of your favorite?' onChange={(ev)=>{setFavorite(ev.target.value)}}  name="fav">is one of your favorite?</Form.Check>}
+                </Form.Group>
+            </Row>
+            <Row>
+                <Form.Group controlId="addButton">
+                    <Form.Label className='fw-light'>&nbsp;</Form.Label><br/>
                     {props.addNewOrEdit=='add' && <Button variant='success' id="addbutton" onClick={handleAdd}>ADD</Button>}
                     {props.addNewOrEdit=='edit' && <Button variant='success' id="addbutton" onClick={handleSave}>SAVE</Button>}
                     {' '}<Button variant='secondary' id="addbutton" onClick={handleCancel}>CANCEL</Button>
                 </Form.Group>
-                </td>
-            </tr>
+            </Row>
+            </td>
         </tbody>
-    </table>
+    </Table>
 }
 
 export {EditOrNewFilm}
